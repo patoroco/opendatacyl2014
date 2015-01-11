8e36a21c8ccc581f3c33ef5dd913531ec5689a16
Jorge Maroto <patoroco@gmail.com>
2015-01-11 14:20:05 +0100

    Load cartomap

---
+++ js/map.js
@@ -0,0 +1,15 @@
+var map;
+
+var SQL_API_URL = 'https://jorge.cartodb.com/api/v2/sql';
+var VIZ_URL = 'http://jorge.cartodb.com/api/v2/viz/7966eb1c-998e-11e4-b96f-0e4fddd5de28/viz.json';
+
+function init_map() {
+    cartodb.createVis('map', VIZ_URL)
+    .done(function(vis, layers) {
+        map = vis.getNativeMap();
+    });
+}
+
+function change_center(lat_center, lon_center) {
+    map.panTo({lat: lat_center, lng: lon_center});
+}
