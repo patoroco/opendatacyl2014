6c78bdf7c822f1b3bdc6e3538d271d844d146a70
nhpatt <nhpatt@gmail.com>
2015-01-15 23:50:32 +0100

    evolution map!

--- js/evolution.controller.js
+++ js/evolution.controller.js
@@ -4,65 +4,61 @@
         .controller('EvolutionController', ['$scope', '$rootScope', '$interval', EvolutionController]);
 
     function EvolutionController($scope, $rootScope, $interval) {
+        var viz = 'http://opendatacyl.cartodb.com/api/v2/viz/ebbaa494-9c16-11e4-80c2-0e0c41326911/viz.json';
+        var TILES_URL = 'https://cartocdn_{s}.global.ssl.fastly.net/base-eco/{z}/{x}/{y}.png';
+        var layer;
         var vm = this;
         vm.enableAnimation = enableAnimation;
         vm.disableAnimation = disableAnimation;
+        vm.dates = ['09/01/14', '06/01/14', '03/01/14', '12/01/13', '09/01/13', '06/01/13', '03/01/13', '12/01/13', '09/01/13', '06/01/13', '03/01/13', '12/01/11', '09/01/11', '06/01/11', '03/01/11', '12/01/10', '09/01/10', '06/01/10', '03/01/10', '12/01/09', '09/01/09', '06/01/09', '03/01/09', '12/01/08', '09/01/08', '06/01/08', '03/01/08', '12/01/07', '09/01/07', '06/01/07', '03/01/07', '12/01/06', '09/01/06', '06/01/06', '03/01/06', '12/01/05', '09/01/05', '06/01/05', '03/01/05', '12/01/04', '09/01/04', '06/01/04', '03/01/04', '12/01/03', '09/01/03', '06/01/03', '03/01/03', '12/01/02', '09/01/02', '06/01/02', '03/01/02'].reverse();
+        vm.value = vm.dates.length - 1;
+        vm.date = vm.dates[vm.value];
 
         $rootScope.evolution = true;
-
 
         var unemployment_map = new L.Map('unemployment_map', {
             zoom: 7,
             center: PROVINCE_COORD['Palencia'],
-            minZoom: 3,
-            maxZoom: 15
+            minZoom: 6,
+            maxZoom: 8
         });
-        var TILES_URL = 'https://cartocdn_{s}.global.ssl.fastly.net/base-eco/{z}/{x}/{y}.png';
-        vm.date = '09/01/14';
+
+        //FIXME refactor this plz
 
         $scope.$watch('$viewContentLoaded', function () {
-
             L.tileLayer(TILES_URL).addTo(unemployment_map);
-            cartodb.createLayer(unemployment_map, {
-                user_name: 'opendatacyl',
-                type: 'cartodb',
-                sublayers: [{
-                    sql: "SELECT * FROM empleo WHERE fecha = '09/01/14'",
-                    cartocss: cartocss
-                }]
-            }).addTo(unemployment_map)
-                .done(function (layer) {
-
-                    executeSQL(vm.date);
-                    $scope.$watch('evolution.value', function (value, oldValue) {
-                        if (value && value != oldValue) {
-                            vm.date = vm.dates[value >= vm.dates.length ? 0 : value];
-                            executeSQL(vm.date);
-                            layer.getSubLayer(0).setSQL("SELECT * FROM empleo WHERE fecha = '" + vm.date + "'");
-                        }
-                    });
-
+            cartodb.createLayer(unemployment_map, viz).addTo(unemployment_map)
+                .done(function (layerLoaded) {
+                    layer = layerLoaded;
+                    executeSQL(vm.value);
                 });
-
-            function executeSQL(date) {
-                var sql = new cartodb.SQL({ user: 'opendatacyl' });
-                sql.execute("SELECT * FROM empleo WHERE fecha = '" + date + "'")
-                    .done(function(data) {
-                        vm.unemployments = data;
-                    })
-                    .error(function(errors) {
-                        // errors contains a list of errors
-                        console.log("errors:" + errors);
-                    })
-            }
-
         });
 
-        vm.dates = ['09/01/14', '06/01/14', '03/01/14', '12/01/13', '09/01/13', '06/01/13', '03/01/13', '12/01/13', '09/01/13', '06/01/13', '03/01/13', '12/01/11', '09/01/11', '06/01/11', '03/01/11', '12/01/10', '09/01/10', '06/01/10', '03/01/10', '12/01/09', '09/01/09', '06/01/09', '03/01/09', '12/01/08', '09/01/08', '06/01/08', '03/01/08', '12/01/07', '09/01/07', '06/01/07', '03/01/07', '12/01/06', '09/01/06', '06/01/06', '03/01/06', '12/01/05', '09/01/05', '06/01/05', '03/01/05', '12/01/04', '09/01/04', '06/01/04', '03/01/04', '12/01/03', '09/01/03', '06/01/03', '03/01/03', '12/01/02', '09/01/02', '06/01/02', '03/01/02'];
+        $scope.$watch('evolution.value', function (value, oldValue) {
+            if (value != null && value != oldValue) {
+                executeSQL(value);
+                var query = "SELECT * FROM empleo WHERE fecha = '" + vm.date + "'"
+                layer.getSubLayer(0).setSQL(query);
+            }
+        });
+
+        function executeSQL(value) {
+            vm.date = vm.dates[value >= vm.dates.length ? 0 : value];
+            var sql = new cartodb.SQL({user: 'opendatacyl'});
+            var query = "SELECT * FROM empleo WHERE fecha = '" + vm.date + "'"
+            sql.execute(query)
+                .done(function (data) {
+                    vm.unemployments = data;
+                    $scope.$digest();
+                })
+                .error(function (errors) {
+                    console.log("errors:" + errors);
+                });
+        }
 
         function enableAnimation() {
             vm.interval = $interval(function () {
-                vm.value = vm.value != null ? (vm.value >= vm.dates.length ? 0 : (vm.value + 1)) : 0;
+                vm.value = vm.value >= vm.dates.length ? 0 : (vm.value + 1);
             }, 1000);
         }
 
@@ -70,10 +66,6 @@
             $interval.cancel(vm.interval);
         }
     }
-
-
-    var cartocss = '#empleo{ polygon-fill: #FFFFB2; polygon-opacity: 0.8; line-color: #FFF; line-width: 1; line-opacity: 1; } #empleo [ paro <= 26.7605633802817] { polygon-fill: #B10026; } #empleo [ paro <= 23.5609103078983] { polygon-fill: #E31A1C; } #empleo [ paro <= 22.4755700325733] { polygon-fill: #FC4E2A; } #empleo [ paro <= 21.5789473684211] { polygon-fill: #FD8D3C; } #empleo [ paro <= 18.0868609125893] { polygon-fill: #FEB24C; } #empleo [ paro <= 16.9300225733634] { polygon-fill: #FED976; } #empleo [ paro <= 15.8478605388273] { polygon-fill: #FFFFB2; }';
-
 
 })();
 
